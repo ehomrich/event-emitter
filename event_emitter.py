0e70188ef353b6bd8f0de5ab797d9a009c9983ba
Émerson P. Homrich <12125514+ehomrich@users.noreply.github.com>
2019-10-29 01:37:16 -0300

    Add max listeners check and private helper method

--- event_emitter.py
+++ event_emitter.py
@@ -1,3 +1,4 @@
+from warnings import warn
 from collections import defaultdict
 from functools import wraps
 from typing import DefaultDict, Optional, Tuple, Union, List, Callable, Any
@@ -5,24 +6,37 @@ from typing import DefaultDict, Optional, Tuple, Union, List, Callable, Any
 DEFAULT_MAX_LISTENERS = 10
 
 
+class MaxListenersExceededWarning(UserWarning):
+    pass
+
+
+def _warn_possible_memory_leak(event: str, count: int) -> None:
+    warn(
+        'Possible EventEmitter memory leak detected. '
+        f"{count} '{event}' listeners added. "
+        'Use emitter.set_max_listeners() to increase limit.',
+        MaxListenersExceededWarning
+    )
+
+
 class EventEmitter:
     def __init__(self, max_listeners: Optional[int] = None) -> None:
         self._events: DefaultDict[str, list] = defaultdict(list)
-        self.set_max_listeners(max_listeners or DEFAULT_MAX_LISTENERS)
+        self.set_max_listeners(max_listeners if max_listeners is not None else DEFAULT_MAX_LISTENERS)
 
     def __len__(self) -> int:
         return len(self._events)
 
-    def __getitem__(self, key) -> list:
+    def __getitem__(self, key: str) -> list:
         if key not in self._events:
             raise KeyError(key)
 
         return self._events.get(key)
 
-    def __delitem__(self, key) -> None:
+    def __delitem__(self, key: str) -> None:
         del self._events[key]
 
-    def __contains__(self, key) -> bool:
+    def __contains__(self, key: str) -> bool:
         return key in self._events
 
     @property
@@ -55,9 +69,22 @@ class EventEmitter:
     def count(self, event: str) -> int:
         return len(self._get_listeners(event))
 
-    def add_listener(self, event: str, handler: Callable) -> None:
+    def full(self, event: str) -> bool:
+        return self.count(event) >= self.max_listeners
+
+    def _add_listener(self, event: str, handler: Callable, *, prepend: bool = False) -> None:
+        if prepend:
+            self._events[event].insert(0, handler)
+        else:
+            self._events[event].append(handler)
+
         self.emit('new_listener', event, handler)
-        self._events[event].append(handler)
+
+        if self.full(event):
+            _warn_possible_memory_leak(event, self.count(event))
+
+    def add_listener(self, event: str, handler: Callable) -> None:
+        self._add_listener(event, handler, prepend=False)
 
     def on(self, event: str, handler: Optional[Callable] = None) -> Callable:
         def wrapper(fn: Callable) -> Callable:
@@ -70,8 +97,7 @@ class EventEmitter:
         return wrapper
 
     def preprend_listener(self, event: str, handler: Callable) -> None:
-        self.emit('new_listener', event, handler)
-        self._events[event].insert(0, handler)
+        self._add_listener(event, handler, prepend=True)
 
     def remove_listener(self, event: str, handler: Callable) -> None:
         if handler in self._get_listeners(event):
@@ -92,7 +118,7 @@ class EventEmitter:
             @wraps(fn)
             def remove_then_run(*args: Any, **kwargs: Any) -> Any:
                 self.remove_listener(event, remove_then_run)
-                fn(*args, **kwargs)
+                return fn(*args, **kwargs)
 
             self.add_listener(event, remove_then_run)
             return remove_then_run
